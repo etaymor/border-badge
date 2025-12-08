70775544d1d7c83d4e9dd5efc168c1975c14278d
Emerson Taymor <emerson.taymor@gmail.com>
2025-12-07 20:41:40 -0500

    google fonts and missing stamps

---
+++ mobile/src/constants/typography.ts
@@ -0,0 +1,14 @@
+export const fonts = {
+  playfair: {
+    regular: 'PlayfairDisplay_400Regular',
+    bold: 'PlayfairDisplay_700Bold',
+  },
+  dawning: {
+    regular: 'DawningofaNewDay_400Regular',
+  },
+  openSans: {
+    regular: 'OpenSans_400Regular',
+    semiBold: 'OpenSans_600SemiBold',
+    bold: 'OpenSans_700Bold',
+  },
+} as const;
